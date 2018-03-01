b99fa9541a2b7135811ea7bcceaa70feed9ee656
j3j3y123 <j3j3y123@naver.com>
2018-03-01 21:10:49 +0900

    블루투스 권한 추가

---
+++ app/src/main/java/school/high/andong/ftminesweeper/IntroActivity.java
@@ -0,0 +1,17 @@
+package school.high.andong.ftminesweeper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by j3j3y on 2018-03-01.
+ */
+
+public class IntroActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
--- app/src/main/java/school/high/andong/ftminesweeper/MainActivity.java
+++ app/src/main/java/school/high/andong/ftminesweeper/MainActivity.java
@@ -1,13 +1,61 @@
 package school.high.andong.ftminesweeper;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Set;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null) {
+            //장치가 블루투스를 지원하지 않는 경우.
+        }
+
+        else {
+            if(!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+            else {
+                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+                if(pairedDevices.size() > 0) {
+                    // 페어링 된 장치가 있는 경우.
+
+                }
+
+                else {
+                    // 페어링 된 장치가 없는 경우.
+                }
+            }
+            // 장치가 블루투스를 지원하는 경우.
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case REQUEST_ENABLE_BT:
+                if(resultCode == RESULT_OK) {
+                    // 블루투스가 활성 상태로 변경됨
+                }
+
+                else if(resultCode == RESULT_CANCELED) {
+                    // 블루투스가 비활성 상태임
+                    finish();  //  어플리케이션 종료
+
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
